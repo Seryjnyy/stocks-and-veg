1a7b13c70f864ab9c8e4b29f2610fd1c8f63eea2
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-10-14 12:45:28 +0200

    Add a leaderboard list component
    - Added a component to reuse across the site.

---
+++ src/components/group/leaderboard-list.tsx
@@ -0,0 +1,49 @@
+import useLeaderboard from "@/hooks/use-leaderboard";
+import { addOrdinalSuffix, cn } from "@/lib/utils";
+import { Skeleton } from "../ui/skeleton";
+import GroupUserProfile from "./group-user-profile";
+
+export default function LeaderboardList({
+    groupID,
+    userID,
+}: {
+    groupID: string;
+    userID: string;
+}) {
+    const { leaderboardData, isLoading, isError } = useLeaderboard({
+        groupID,
+        userID,
+    });
+
+    if (isLoading) return <Skeleton className="w-full h-10" />;
+
+    if (isError || !leaderboardData) return null;
+
+    return (
+        <>
+            <div className="flex flex-col gap-1  w-full items-end">
+                {leaderboardData.getAdjacentUsers(2).map((user) => (
+                    <div
+                        key={user.user.user_id}
+                        className={cn(
+                            "border px- h-fit flex items-center rounded-md w-fit",
+                            user.position == leaderboardData.userPosition &&
+                                "bg-secondary"
+                        )}
+                    >
+                        <span className="text-xs text-muted-foreground  w-[6ch] text-center">
+                            {user.position <= 3
+                                ? addOrdinalSuffix(user.position)
+                                : user.position}
+                        </span>
+                        <GroupUserProfile
+                            groupUser={user.user}
+                            avatarSize={"md"}
+                            detailSize={"xl"}
+                        />
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
